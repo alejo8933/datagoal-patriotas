e6040e21e9efb2787260f5501a6510dfa6c0443b
ProfeAlbeiro <profealbeiro2020@gmail.com>
2026-04-09 12:30:58 -0500

    chore: limpieza de archivos duplicados y documentacion obsoleta

--- src/services/statisticsService.ts
+++
@@ -1,8 +0,0 @@
-import { SupabaseStatisticsRepository } from '@/repositories/supabase/StatisticsRepository'
-
-const repo = new SupabaseStatisticsRepository()
-
-export const statisticsService = {
-  getGoalscorers:     () => repo.getGoalscorers(),
-  getTeamPerformance: () => repo.getTeamPerformance(),
-}
